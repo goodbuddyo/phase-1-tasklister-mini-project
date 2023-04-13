17c6c3e0509cddd1ad74bf686cf93a5b63b55937
Seano <sean01234@yahoo.com>
2023-04-13 12:24:37 -0400

    add tasks to form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+//document.addEventListener("DOMContentLoaded",() => {
+//  // your code here
+//});
+
+document.addEventListener('DOMContentLoaded',() => {
+  let form=document.querySelector('form').addEventListener('submit',(e) => {
+    e.preventDefault()
+    //console.log(e.target)
+    //console.log(`e.target ${e.target}`)
+    //console.log(`e.target.new_task_description ${e.target.new_task_description}`)
+    //console.log(`e.target.new_task_description.value ${e.target.new_task_description.value}`)
+
+    buildToDo(e.target.new_task_description.value)
+
+  })
+})
+
+
+function buildToDo(todo) {
+  let li=document.createElement('li')
+  li.textContent=todo
+  document.querySelector('#tasks').appendChild(li)
+  console.log(li)
+}
+
+
